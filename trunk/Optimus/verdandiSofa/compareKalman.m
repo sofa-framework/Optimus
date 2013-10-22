77c342a1c00fcaaac1d4c638a64674f904556201
igor <igor@3bf0124e-f681-4360-acf1-5e196a5857e9>
2013-10-22 16:11:45 +0000

    ADD: matlab file to visualize estimation
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/optimus@52 3bf0124e-f681-4360-acf1-5e196a5857e9

---
+++ trunk/Optimus/verdandiSofa/compareKalman.m
@@ -0,0 +1,66 @@
+clear all
+%close all
+
+%it=10; nParam=2; nColl=2; nDim=1;
+%indir='/home/ipeterlik/Work/Sofa/applications-dev/plugins/Optimus/verdandiSofa/verdandi-1.5/example/clamped_bar/result'
+
+it=10; nParam=2; nColl=1; nDim=3;
+indir='/home/ipeterlik/Work/Sofa/applications-dev/plugins/Optimus/verdandiSofa/scenes/result2_1673';
+
+
+%T=load(sprintf('%s/truth-forecast_state.dat',indir));
+%P=load(sprintf('%s/ukf-forecast_state.dat',indir));
+%A=load(sprintf('%s/ukf-analysis_state.dat',indir));
+R=load(sprintf('%s/roukf-forecast_state.dat',indir));
+
+nState=size(R,2)
+nObs=size(R,1)
+
+nDof=(nState-nParam)/nColl;
+nNode=nDof/nDim;
+
+desiredObs = nObs;
+
+figure; plot(1:desiredObs, R(1:desiredObs,nState-1), 1:desiredObs, R(1:desiredObs,nState));
+%figure; plot(1:nObs, R(:,nState-2), 1:nObs, R(:,nState-1), 1:nObs, R(:,nState));
+
+fprintf('Parameters found: %f %f\n', R(nObs, nState-1), R(nObs,nState));
+
+
+return
+
+t=reshape(T(it,1:nDof), nDim, nNode)';
+p=reshape(P(it,1:nDof), nDim, nNode)';
+a=reshape(A(it,1:nDof), nDim, nNode)';
+
+return
+
+if (nDim == 3) 
+    figure; 
+    hold on
+    daspect([1 1 1]);
+    scatter3(t(:,1), t(:,2), t(:,3),'.r', 'MarkerSize',5);
+    scatter3(p(:,1), p(:,2), p(:,3),'*g');
+    scatter3(a(:,1), a(:,2), a(:,3),'ob');
+end
+
+if (nDim == 1)
+    figure; 
+    hold on
+    %daspect([1 1 1]);
+    plot(t, 'r+', 'MarkerSize',10);
+    plot(p, 'g*');
+    plot(a, 'bo');
+    
+end
+
+figure; 
+%plot(A(:,nState-1));
+%plot(A(:,nState-1));
+
+    
+
+
+
+
+
