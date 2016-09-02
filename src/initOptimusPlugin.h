56721a6a463a190d8ea042d9d3b60d606a3087c8
Igor Peterlik <peterlik@gmail.com>
2016-09-02 12:05:50 +0200

    CHANGE: definition of pthread barriers for apple

--- src/initOptimusPlugin.h
+++ src/initOptimusPlugin.h
@@ -27,6 +27,20 @@
 
 
 #include <sofa/helper/system/config.h>
+#include <pthread.h> /* pthread_t, pthread_barrier_t */
+#include <sys/errno.h>
+
+#ifdef __APPLE__
+typedef int pthread_barrierattr_t;
+typedef struct
+{
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+    int count;
+    int tripCount;
+} pthread_barrier_t;
+
+#endif
 
 #ifdef SOFA_BUILD_OPTIMUSPLUGIN
 #define SOFA_OptimusPlugin_API SOFA_EXPORT_DYNAMIC_LIBRARY
@@ -80,8 +94,6 @@ for (size_t i = 0; i < MAT.rows(); i++) \
         sm += fabs(VEC(i)); \
     std::cout << "@@@ ABS. SUM OF " << NAME << ": " << sm << std::endl; \
 } \
-
-
 
 /** \mainpage
   This plugin contains a set of Optimization Methods (initially mainly Bayesian Filtering techniques)
