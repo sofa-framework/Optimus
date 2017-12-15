d5168a6e28771118f192ba493de3e566f7dee456
Sergei <sergei.nikolaev@inria.fr>
2017-12-15 17:43:28 +0100

    add visualisation of groundtruth values and innovations

--- scenes/assimStiffness/davisu.py
+++ scenes/assimStiffness/davisu.py
@@ -2,6 +2,7 @@ from DAOptions import DAOptions
 import numpy
 import types
 import math
+import sys
 import matplotlib.pyplot as plt
 from itertools import repeat
 
@@ -21,8 +22,16 @@ def load_matrix_from_file(f):
 
     raise TypeError('f must be a file object or a file name.')
 
-
-folder='outCyl3_770_P1_pull_ROUKF_1'
+try : 
+    sys.argv[0]
+except :
+    commandLineArguments = []
+else :
+    commandLineArguments = sys.argv
+if (len(commandLineArguments) > 1):
+    folder=commandLineArguments[1]
+else:
+    folder='outCyl3_770_P1_pull_ROUKF_1'
 
 options = DAOptions()
 options.parseYaml(folder+'/daconfig.yml')
@@ -31,7 +40,8 @@ stateExpVal = load_matrix_from_file(options.export.stateExpValFile)
 stateVar = load_matrix_from_file(options.export.stateVarFile)
 
 
-plt.figure()
+# plot stiffnesses with variances
+plt.figure(1)
 
 nparams=numpy.size(stateVar[1,:])
 nsteps=numpy.size(stateVar[:,1])
@@ -45,6 +55,10 @@ rng=[i*options.model.dt for i in rng]
 
 cmap = plt.cm.get_cmap('hsv', nparams+1)
 
+groundTruthStr = options.observations.groundTruth
+groundTruthValues = groundTruthStr.split(' ')
+data = numpy.ones(nsteps);
+
 for i in range(0,nparams):
 	ev = stateExpVal[:,i]
 	var = stateVar[:,i]
@@ -56,18 +70,32 @@ for i in range(0,nparams):
 	plt.plot(rng, vll, color=cmap(i),  linestyle='dashed')
 	plt.plot(rng, vlu, color=cmap(i),  linestyle='dashed')
 
-	print options.observations.groundTruth
+	# print options.observations.groundTruth
 	#, rng, vll, rng, vlu)
+        
+        groundTruthData = [numpy.int(elem) * groundTruthValues[i] for elem in data]
+        plt.plot(rng, groundTruthData, color=cmap(i), linestyle='None', marker=r'$\clubsuit$', markersize=5)
+	# plt.setp(lines, color=cmap(i), linewidth=2.0)
 
-	#plt.setp(lines, color=cmap(i), linewidth=2.0)
+# plot innovation values
+if options.export.internalData == 1:
+    innovationVal = load_matrix_from_file(options.export.innovationFile)
 
+    plt.figure(2)
+    
+    nparams=numpy.size(innovationVal[1,:])
+    nsteps=numpy.size(innovationVal[:,1])
 
-plt.show()	
+    rng=xrange(0,nsteps)
+    rng=[i*options.model.dt for i in rng]
 
+    cmap = plt.cm.get_cmap('hsv', nparams+1)
 
+    for i in range(0,nparams):
+	innov = innovationVal[:,i]
+	plt.plot(rng, innov, color=cmap(i),  linestyle='solid')
 
-
-
-
+	
+plt.show()
 
 
