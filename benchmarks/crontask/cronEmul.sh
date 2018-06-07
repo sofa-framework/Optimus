a747b7305ee2f3cabc5a4e57f44c26c1c6381706
snikolae <sergei.nikolaev@inria.fr>
2018-06-07 19:01:10 +0200

    ADD: add command for rc.local script

--- benchmarks/crontask/cronEmul.sh
+++ benchmarks/crontask/cronEmul.sh
@@ -12,3 +12,6 @@ done &
 
 # cron command
 # 00 12 * * 1-5 /home/sergei/Optimus_test/runOptimusTests.sh >> /home/sergei/Optimus_test/log_`/bin/date +"\%d_\%m_\%Y"`.txt
+
+# rc.local command
+# exec setuidgid sergei /bin/bash /home/sergei/Optimus_test/cronEmul.sh
