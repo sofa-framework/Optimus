e0e456414531c95d3ad6ee7dbd2a8ec9988860d4
snikolae <sergei.nikolaev@inria.fr>
2018-06-18 13:41:03 +0200

    FIX: replace day and year in file name for better files sorting

--- benchmarks/crontask/cronEmul.sh
+++ benchmarks/crontask/cronEmul.sh
@@ -3,7 +3,7 @@
 while :; do
    currenttime=$(date +%H:%M)
    if [[ "$currenttime" > "12:01" ]] && [[ "$currenttime" < "12:07" ]]; then
-     /bin/bash /home/sergei/Optimus_test/runOptimusTests.sh >> /home/sergei/Optimus_test/log_`/bin/date +"%d_%m_%Y"`.txt
+     /bin/bash /home/sergei/Optimus_test/runOptimusTests.sh >> /home/sergei/Optimus_test/log_`/bin/date +"%Y_%m_%d"`.txt
      sleep 360s
    else
      sleep 50s
@@ -11,7 +11,7 @@ while :; do
 done &
 
 # cron command
-# 00 12 * * 1-5 /home/sergei/Optimus_test/runOptimusTests.sh >> /home/sergei/Optimus_test/log_`/bin/date +"\%d_\%m_\%Y"`.txt
+# 00 12 * * 1-5 /home/sergei/Optimus_test/runOptimusTests.sh >> /home/sergei/Optimus_test/log_`/bin/date +"\%Y_\%m_\%d"`.txt
 
 # rc.local command
 # exec setuidgid sergei /bin/bash /home/sergei/Optimus_test/cronEmul.sh
