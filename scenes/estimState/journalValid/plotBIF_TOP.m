3f9c084c9c79a0d03ca31db17795a625fd2f6b97
raffa <raffaella.trivisonne@inria.fr>
2018-07-01 17:08:55 +0200

    up

--- scenes/estimState/journalValid/plotBIF_TOP.m
+++ scenes/estimState/journalValid/plotBIF_TOP.m
@@ -25,26 +25,28 @@ load TOP_hd2d_2.mat
 load TOP_hd2d_3.mat 
 
 figure(5)
-plot(TOP_RMSE_0*1000)
+plot(TOP_RMSE_0*1000,'LineWidth',1.5) %blu
 hold on
-plot(TOP_RMSE_1*1000, 'Color',[1 0 0])
+plot(TOP_RMSE_1*1000,'LineWidth',1.5, 'Color',[0.600000023841858 0 0]) %rosso
 hold on
-plot(TOP_RMSE_2*1000, 'Color',[0 0.8 0])
+plot(TOP_RMSE_2*1000,'LineWidth',1.5, 'Color',[0 0.600000023841858 0]) %verde
 hold on
-plot(TOP_RMSE_3*1000, 'Color',[1 0 1])
+plot(TOP_RMSE_3*1000,'LineWidth',1.5, 'Color',[0.87058824300766 0.490196079015732 0]) %giallo
 grid on
+grid minor
 xlabel('h - [simulation step]') % x-axis label
 ylabel('RMSE [mm] ') % y-axis label
 title( {' 3D Ground Truth Tip - 3D  Filter Tip';'Root Mean Square Error'  }) ;
 
 figure(6)
-plot(TOP_RMSE2d_0)
+plot(TOP_RMSE2d_0,'LineWidth',1.5)
 hold on
-plot(TOP_RMSE2d_1, 'Color',[1 0 0]) 
+plot(TOP_RMSE2d_1, 'LineWidth',1.5, 'Color',[0.600000023841858 0 0]) %rosso 
 hold on
-plot(TOP_RMSE2d_2, 'Color',[0 0.8 0])
+plot(TOP_RMSE2d_2, 'LineWidth',1.5, 'Color',[0 0.600000023841858 0])
+%verde 
 hold on
-plot(TOP_RMSE2d_3, 'Color',[1 0 1])
+plot(TOP_RMSE2d_3, 'LineWidth',1.5, 'Color',[0.87058824300766 0.490196079015732 0]) %giallo
 grid on
 xlabel('h - [simulation step]') % x-axis label
 ylabel('RMSE [pixel] ') % y-axis label
@@ -52,27 +54,29 @@ title( {'2D Ground Truth Tip - 2D  Filter Tip'; ' Root Mean Square Error' }) ;
 
 
 figure(7)
-plot(TOP_hd_0*1000)
+plot(TOP_hd_0*1000,'LineWidth',1.5)
 hold on
-plot(TOP_hd_1*1000, 'Color',[1 0 0])
+plot(TOP_hd_1*1000, 'LineWidth',1.5, 'Color',[0.600000023841858 0 0]) %rosso)
 hold on
-plot(TOP_hd_2*1000, 'Color',[0 0.8 0])
+plot(TOP_hd_2*1000, 'LineWidth',1.5, 'Color',[0 0.600000023841858 0])
 hold on
-plot(TOP_hd_3*1000, 'Color',[1 0 1])
+plot(TOP_hd_3*1000, 'LineWidth',1.5, 'Color',[0.87058824300766 0.490196079015732 0])
 grid on
+grid minor
 xlabel('h - [simulation step]') % x-axis label
 ylabel('Hausdorff [mm] ') % y-axis label
 title( {'Ground Truth Catheter - Filter Catheter';'Hausdorff Distance'  }) ;
 
 figure(8)
-plot(TOP_hd2d_0)
+plot(TOP_hd2d_0,'LineWidth',1.5)
 hold on
-plot(TOP_hd2d_1, 'Color',[1 0 0])
+plot(TOP_hd2d_1, 'LineWidth',1.5, 'Color',[0.600000023841858 0 0]) %rosso)
 hold on
-plot(TOP_hd2d_2, 'Color',[0 0.8 0])
+plot(TOP_hd2d_2, 'LineWidth',1.5, 'Color',[0 0.600000023841858 0])
 hold on
-plot(TOP_hd2d_3, 'Color',[1 0 1])
+plot(TOP_hd2d_3, 'LineWidth',1.5, 'Color',[0.87058824300766 0.490196079015732 0])
 grid on
+grid minor
 xlabel('h - [simulation step]') % x-axis label
 ylabel('Hausdorff [pixel] ') % y-axis label
 title( {'2D  Ground Truth Catheter-  2D Filter Catheter';'Hausdorff Distance' }) ;
