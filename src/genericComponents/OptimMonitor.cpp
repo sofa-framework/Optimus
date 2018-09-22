487a271e7af29decdc68ed8e7a466efdc71b4f05
Igor Peterlik <peterlik@gmail.com>
2018-09-22 11:04:09 +0200

    ADD: my own monitor (since the one in SOFA is far too crappy)

---
+++ src/genericComponents/OptimMonitor.cpp
@@ -0,0 +1,66 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, development version     *
+*                (c) 2006-2018 INRIA, USTL, UJF, CNRS, MGH                    *
+*                                                                             *
+* This program is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+*******************************************************************************
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#define SOFA_COMPONENT_MISC_OPTIMMONITOR_CPP
+
+#include "OptimMonitor.inl"
+#include <sofa/defaulttype/VecTypes.h>
+#include <sofa/defaulttype/RigidTypes.h>
+#include <sofa/core/ObjectFactory.h>
+
+namespace sofa
+{
+namespace component
+{
+namespace misc
+{
+SOFA_DECL_CLASS(OptimMonitor)
+
+using namespace sofa::defaulttype;
+
+// Register in the Factory
+int OptimMonitorClass = core::RegisterObject("OptimMonitoring of particles")
+        #ifndef SOFA_FLOAT
+        .add< OptimMonitor<Vec3dTypes> >(true)
+        .add< OptimMonitor<Vec6dTypes> >()
+        .add< OptimMonitor<Rigid3dTypes> >()
+        #endif
+        #ifndef SOFA_DOUBLE
+        .add< OptimMonitor<Vec3fTypes> >()
+        .add< OptimMonitor<Vec6fTypes> >()
+        .add< OptimMonitor<Rigid3fTypes> >()
+        #endif
+        ;
+
+#ifndef SOFA_FLOAT
+template class SOFA_VALIDATION_API OptimMonitor<Vec3dTypes>;
+template class SOFA_VALIDATION_API OptimMonitor<Vec6dTypes>;
+template class SOFA_VALIDATION_API OptimMonitor<Rigid3dTypes>;
+#endif
+#ifndef SOFA_DOUBLE
+template class SOFA_VALIDATION_API OptimMonitor<Vec3fTypes>;
+template class SOFA_VALIDATION_API OptimMonitor<Vec6fTypes>;
+template class SOFA_VALIDATION_API OptimMonitor<Rigid3fTypes>;
+#endif
+
+} // namespace misc
+} // namespace component
+} // namespace sofa
