8124f017283cdddc86eebc7c84d10935841e719f
snikolae <sergei.nikolaev@inria.fr>
2018-12-14 14:36:31 +0100

    ADD: add benchmark test for polynomial springs

---
+++ benchmarks/assimBC_liver_polynomial_springs_point_cloud_yaml/verify.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if [ $# == "1" ]; then
+        SOFA_EXEC=$1
+else
+        SOFA_EXEC=runSofa
+fi
+echo "Using SOFA executable: " $SOFA_EXEC
+
+numItSDA=8000
+goScene=liver_controlPoint_nonlinear_BC_point_cloud_scene_GenObs_bench.py
+sdaScene=liver_controlPoint_nonlinear_BC_point_cloud_scene_SDA_bench.py
+yamlConfig=liver_nonlinear_BC_point_cloud_scene_config_bench.yml
+compareScript=../helper/compareData.py
+
+#==============================================================
+
+numItObs=$[$numItSDA+1]
+
+rm -rf obs_testing
+mkdir -p obs_testing
+
+rm -rf roukf_testing
+mkdir -p roukf_testing
+
+echo "Generating observations..."
+$SOFA_EXEC -g batch -n $numItObs $goScene --argv $yamlConfig  &> genObsOut
+echo "... done"
+
+echo "Running data assimilation..."
+$SOFA_EXEC -g batch -n $numItSDA $sdaScene --argv $yamlConfig &> sdaOut
+echo "... done"
+
+echo "Comparing state w.r.t. benchmark:"
+python $compareScript roukf_bench/state_test.txt roukf_testing/state_test.txt $numItSDA
+
+echo "Comparing variance w.r.t. benchmark:"
+python $compareScript roukf_bench/variance_test.txt roukf_testing/variance_test.txt $numItSDA
+
+echo "Comparing covariance w.r.t. benchmark:"
+python $compareScript roukf_bench/covariance_test.txt roukf_testing/covariance_test.txt $numItSDA
