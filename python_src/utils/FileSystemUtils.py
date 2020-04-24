12b513068f5c3d29fedecccc9066ceec7383075b
Sergei <sergei@sergei-MIMESIS-team.com>
2020-04-24 17:16:00 +0200

    FIX: correct python utils to make compatibility with python3

--- python_src/utils/FileSystemUtils.py
+++ python_src/utils/FileSystemUtils.py
@@ -14,14 +14,14 @@ class FolderHandler:
     ###
     def createFolder(self, generalFolderName, folderName, archiveResults):
         self.folder = generalFolderName + '/' + folderName
-        print "Storing the data to: ", self.folder            
+        #print "Storing the data to: ", self.folder
 
         if (os.path.isdir(self.folder)):
             if (archiveResults):
                 if not os.path.isdir(generalFolderName + '/archive'):
                     os.mkdir(generalFolderName + '/archive')
                 archFolder = generalFolderName + '/archive/' + folderName + datetime.datetime.now().strftime("%Y_%b_%d-%I:%M-%S")
-                print "Archiving existing results to archive: ", archFolder
+                #print "Archiving existing results to archive: ", archFolder
                 shutil.move(self.folder, archFolder)
 
             else:
