f18293dcce21a7b26e43e9da943c0ac22a9b78e2
Sergei <sergei@sergei-MIMESIS-team.com>
2020-10-01 16:44:47 +0200

    FIX: add external Eigen directory

--- benchmarks/crontask/runOptimusTests.sh
+++ benchmarks/crontask/runOptimusTests.sh
@@ -97,7 +97,7 @@ fi
 ### configure and make the system
 echo "Recompile sofa sources"
 cd $BUILD_DIRECTORY
-/usr/local/bin/cmake -DSOFA_BUILD_TESTS=ON -DSOFAGUI_BUILD_TESTS=ON -DSOFA_EXTERNAL_DIRECTORIES=/home/sergei/Optimus_test/sofaconfig/sergei .. 2>&1 >> $GENERAL_DIRECTORY/log_`/bin/date +"%Y_%m_%d"`.txt
+/usr/local/bin/cmake -DSOFA_BUILD_TESTS=ON -DSOFAGUI_BUILD_TESTS=ON -DEigen3_DIR=/usr/local/share/eigen3/cmake -DSOFA_EXTERNAL_DIRECTORIES=/home/sergei/Optimus_test/sofaconfig/sergei .. 2>&1 >> $GENERAL_DIRECTORY/log_`/bin/date +"%Y_%m_%d"`.txt
 /usr/bin/make -B -j 8 2>&1 >> $GENERAL_DIRECTORY/log_`/bin/date +"%Y_%m_%d"`.txt
 /usr/bin/make install 2>&1 >> $GENERAL_DIRECTORY/log_`/bin/date +"%Y_%m_%d"`.txt
 
