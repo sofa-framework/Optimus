c6efdac7c5e0a28450eda1f3c68c726b774a25ad
SergeiNikolaev <ser.niev@gmail.com>
2022-06-01 23:47:04 +0200

    FIX: correct preprocessor macros

--- src/stochasticFiltering/ROUKFilter.h
+++ src/stochasticFiltering/ROUKFilter.h
@@ -57,7 +57,7 @@ namespace stochastic
 
 
 
-#ifdef BLAS_INCLUDED
+#ifdef BLAS_LINKED
 /// to speed up, wrappers for BLAS matrix multiplications created, much faster that Eigen by default
 extern "C" {
     // product C= alphaA.B + betaC
@@ -70,7 +70,7 @@ extern "C" {
                double* alpha, double* A, const int* LDA, double* X,
                const int* INCX, double* beta, double* C, const int* INCY);
 }
-#endif // BLAS_INCLUDED
+#endif // BLAS_LINKED
 
 
 
